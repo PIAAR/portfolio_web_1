78b1144388e1d63d10281a98ed18ce900a5e4f13
PIAAR <piaar@rmasters.group>
2022-11-13 17:54:57 -0500

    App edits to list items

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
 const App = () => {
-
     const categories = [
         {
-            id: 1,
-            title : 'Hats',
+            id: '1',
+            title: 'Hats',
         },
         {
-            id: 2,
-            title : 'Jackets',  
+            id: '2',
+            title: 'Jackets',
+        }, 
+        {
+            id: '3',
+            title: 'Sneakers',
         },
         {
-            id: 3,
-            title : 'Sneakers',
+            id: '4',
+            title: 'Womens',
         },
         {
-            id: 4,
-            title : 'Women',
+            id: '5',
+            title: 'Mens',
         },
-        {
-            id: 5,
-            title : 'Men',
-        },
-
-    ];
-
+    ]
     return (
-        <div className="categories-container">
-            { categories.map(({title, id}) => (
-                <div id='id' className="category-container">
-                    <div className="background-image" />
-                    <div className="category-body-container">
-                        <h2>{ title }</h2>
-                        <p>Shop Now</p>
-                    </div>
+        <div className='categories-container'>
+            {categories.map(({ title }) => (
+                <div className='category-container'>
+                {/* <img /> */}
+                <div className='category-body-container'>
+                    <h2>{ title }</h2>
+                    <p>Shop Now</p>
                 </div>
-            ))});
-};
+            </div>
+            ))}
+        </div>
+    );   
+}
 
 export default App;
